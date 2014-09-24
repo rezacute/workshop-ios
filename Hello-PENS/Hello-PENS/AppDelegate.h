35fdb3fe7e7cdc287299c963fd644eee734e97a4
Riza <riza.alaudin@kii.com>
2014-09-24 11:46:11 +0900

    Hello world

---
+++ Hello-PENS/Hello-PENS/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  Hello-PENS
+//
+//  Created by Syah Riza on 9/24/14.
+//  Copyright (c) 2014 Eyro. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ Hello-PENS/Hello-PENS/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  Hello-PENS
+//
+//  Created by Syah Riza on 9/24/14.
+//  Copyright (c) 2014 Eyro. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
